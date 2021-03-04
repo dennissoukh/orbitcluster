472c62e11f7b416d1aa3aa644a1bb3e66d0f1477
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-03-04 13:37:13 +0000

    add services config

---
+++ core/src/config/services.js
@@ -0,0 +1,28 @@
+const { env } = require("../helpers/helpers")
+
+module.exports = {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    mailgun: {
+        domain:     env('MAILGUN_DOMAIN'),
+        secret:     env('MAILGUN_SECRET'),
+        endpoint:   env('MAILGUN_ENDPOINT')
+    },
+
+    spacetrack: {
+        identity:   env('SPACETRACK_USERNAME'),
+        password:   env('SPACETRACK_PASSWORD')
+    }
+
+}
