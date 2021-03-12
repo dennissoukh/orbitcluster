50b7f6ce1d23d6528d5743a2b85cd8042f323fe4
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-03-12 11:16:06 +0000

    add reset password page

--- client/src/views/Auth/passwords/Reset.tsx
+++ client/src/views/Auth/passwords/Reset.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import CSS from 'csstype';
+import { Link } from 'react-router-dom';
+
+const authStyles: CSS.Properties = {
+    width: '500px'
+}
 
 export class ResetPassword extends React.Component {
     render() {
-        return <h1>Reset Password</h1>;
+        return (
+            <div className="d-flex align-items-center justify-content-center min-vh-100">
+                <div className="auth-container" style={authStyles}>
+                    <h1 className="font-bold">Reset Password</h1>
+                    <div className="mt-4">
+                        <form>
+                            <div className="form-group">
+                                <span className="label text-small">Email Address</span>
+                                <input type="text"/>
+                            </div>
+                            <div className="form-group mt-4">
+                                <button className="btn w-100">Send Reset Email</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="mt-4 d-flex justify-content-center">
+                        <div>
+                            <Link to="/login" className="text-small">Remembered your password?</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
