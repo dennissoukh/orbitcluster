ae57b6a6330181c77f75d2f8481195271c372584
Liam Gavigan <L.Gavigan2@nuigalway.ie>
2021-03-13 19:05:28 +0000

    fixing up schemas

--- core/src/database/schemas/User.js
+++ core/src/database/schemas/User.js
@@ -1,57 +1,65 @@
 const data = async (db) => {
-    db.createCollection("user", {
+    db.createCollection('user', {
         validator: {
             $jsonSchema: {
-                bsonType: "object",
-                required: ["name", "email", "user_name", "password", "location", "created_at", "updated_at"],
+                bsonType: 'object',
+                required: ['name', 'email', 'username', 'password', 'location', 'created_at', 'updated_at'],
                 properties: {
                     name: {
-                        bsonType: "object",
-                        required: ["first_name", "last_name"],
+                        bsonType: 'object',
+                        required: ['first_name', 'last_name'],
                         properties: {
                             first_name: {
-                                bsonType: "string",
-                                description: "must be a string and is required"
+                                bsonType: 'string',
+                                description: 'must be a string and is required'
                             },
                             last_name: {
-                                bsonType: "string",
-                                description: "must be a string and is required"
+                                bsonType: 'string',
+                                description: 'must be a string and is required'
                             }
                         }
                     },
                     email: {
-                        bsonType: "string",
-                        description: "must be a string and is required"
+                        bsonType: 'string',
+                        description: 'must be a string and is required'
                     },
-                    user_name: {
-                        bsonType: "string",
-                        description: "must be a string and is required"
+                    username: {
+                        bsonType: 'string',
+                        description: 'must be a string and is required'
                     },
                     password: {
-                        bsonType: "string",
-                        description: "must be a string and is required"
+                        bsonType: 'string',
+                        description: 'must be a string and is required'
                     },
                     location: {
-                        bsonType: "object",
-                        required: ["city"],
+                        bsonType: 'object',
+                        required: ['name', 'latitude', 'longitude', 'elevation'],
                         properties: {
-                            street: {
-                                bsonType: "string",
-                                description: "must be a string if the field exists"
+                            name: {
+                                bsonType: 'string',
+                                description: 'must be a string and is required'
                             },
-                            city: {
-                                bsonType: "string",
-                                description: "must be a string and is required"
+                            latitude: {
+                                bsonType: 'double',
+                                description: 'must be a double and is required'
+                            }
+                            longitude: {
+                                bsonType: 'double',
+                                description: 'must be a double and is required'
+                            },
+                            elevation: {
+                                bsonType: 'double',
+                                description: 'must be a double and is required'
                             }
                         }
                     },
                     created_at: {
-                        bsonType: "date",
-                        description: "must be a date and is automatically provided"
+                        bsonType: 'date',
+                        description: 'must be a date and is automatically provided'
                     },
                     updated_at: {
-                        bsonType: "date",
-                        description: "must be a date and is automatically provided"
+                        bsonType: 'date',
+                        description: 'must be a date and is automatically provided'
                     }
                 }
             }
