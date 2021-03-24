97be605b2258ce6895a66e0e0e558d80a62c5c2d
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-03-24 23:34:44 +0000

    feat: sort Neuron commands using prefix

--- core/src/packages/Neuron/src/Help.js
+++ core/src/packages/Neuron/src/Help.js
@@ -12,7 +12,23 @@ getCommandsForDisplay = (commands, aliases) => {
         });
     });
 
+    commandList = groupCommands(commandList);
+
     return commandList;
+}
+
+groupCommands = (commands) => {
+    return commands.reduce((rv, x) => {
+        if (!x.displayName.includes(':')) {
+            (rv['default'] = rv['default'] || []).push(x);
+            return rv;
+        };
+
+        const namespace = x.displayName.split(':')[0];
+        (rv[namespace] = rv[namespace] || []).push(x);
+
+        return rv;
+    }, {});
 }
 
 printHelp = (commands, flags, aliases) => {
@@ -21,21 +37,47 @@ printHelp = (commands, flags, aliases) => {
     /**
      * Get the length of the longest command.
      */
-    const maxWidth = Math.max(...commandList.map(command => command.width));
+    const maxWidth = Math.max(...Object.values(commandList).map(command => command[0].width));
 
-    console.log(`Orbitcluster Framework ${chalk.green(version)}`);
-    console.log();
+    console.log(`Orbitcluster Framework (Neuron) ${chalk.green(version)}`);
+    console.log('');
 
-    console.log(chalk.yellow('Available commands:'))
-    commandList.forEach((command) => {
-        const displayName = chalk.green(command.displayName);
-        const whitespace = ''.padEnd(maxWidth - command.width, ' ');
-        const description = chalk.white(command.description);
+    console.log(chalk.yellow('Usage:'));
+    console.log('  command [options] [arguments]');
+    console.log('');
 
-        console.log(
-            `  ${displayName} ${whitespace} ${description}`
-        );
-    })
+    console.log(chalk.yellow('Available commands:'));
+
+    // console.log(commandList)
+    commandList.default.forEach((key) => {
+        commandList.default.forEach((command) => {
+            const displayName = chalk.green(command.displayName);
+            const whitespace = ''.padEnd(maxWidth - command.width, ' ');
+            const description = chalk.white(command.description);
+
+            console.log(
+                `  ${displayName} ${whitespace} ${description}`
+            );
+        })
+    });
+
+    Object.keys(commandList).forEach((key) => {
+        if (key === 'default') {
+            return;
+        }
+
+        console.log(chalk.yellow(key))
+
+        commandList[key].forEach((command) => {
+            const displayName = chalk.green(command.displayName);
+            const whitespace = ''.padEnd(maxWidth - command.width, ' ');
+            const description = chalk.white(command.description);
+
+            console.log(
+                `  ${displayName} ${whitespace} ${description}`
+            );
+        })
+    });
 }
 
 module.exports = {
