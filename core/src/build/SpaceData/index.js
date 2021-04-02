7d5a35747472c12bbdf683ce3d9b527e4c294832
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-04-02 22:55:08 +0100

    feat: add SpaceData package

---
+++ core/src/build/SpaceData/index.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * @orbitcluster/SpaceData - Data package for the Orbitcluster application
+ *
+ * (c) 2021 Dennis Soukhikh
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SpaceTrack = void 0;
+var SpaceTrack_1 = require("./src/SpaceTrack");
+Object.defineProperty(exports, "SpaceTrack", { enumerable: true, get: function () { return SpaceTrack_1.SpaceTrack; } });
---
+++ core/src/packages/SpaceData/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @orbitcluster/SpaceData - Data package for the Orbitcluster application
+ *
+ * (c) 2021 Dennis Soukhikh
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { SpaceTrack } from './src/SpaceTrack';
