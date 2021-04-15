08124503e39fa2169a06a2876e607f7ed6b35a07
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-04-15 16:44:07 +0100

    refactor: command error handling

--- core/src/commands/DownloadSatRadio.js
+++ core/src/commands/DownloadSatRadio.js
@@ -65,10 +65,10 @@ class SatRadioDownloader extends BaseCommand {
                 }
             }
         } catch (error) {
-            console.log(
+            console.error(
                 `${Date.now()}> Could not update documents`,
             );
-            console.log(
+            console.error(
                 `${Date.now()}> ${error}`,
             );
         }
