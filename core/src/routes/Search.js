83b8b17c116717d3a8eb8dcb44f62b4005339a98
Liam Gavigan <L.Gavigan2@nuigalway.ie>
2021-04-20 20:39:18 +0100

    refactor: Search.js route

--- core/src/routes/Search.js
+++ core/src/routes/Search.js
@@ -1,7 +1,7 @@
 const routes = async (app) => {
     /**
      * Filter out satellites with a search query
-     */
+     **/
     app.get('/search', {
     }, async (request, reply) => {
         const collection = app.mongo.db.collection('satcat');
