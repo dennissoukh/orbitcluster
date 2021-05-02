67c30f246257e3d4779be943c4c0fc14df9cc054
Liam Gavigan <L.Gavigan2@nuigalway.ie>
2021-05-02 22:48:31 +0100

    feat: add operators routes test

---
+++ core/src/tests/routes/operators.test.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe('/operators', () => {
+    it('checking route properties', async (done) => {
+        chai.request('http://localhost:3000')
+            .get('/v1/operators')
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.have.property('metadata');
+                chai.expect(res.body).to.have.property('data');
+            })
+        done();
+    });
+});
+
+describe('/operators/:id', () => {
+    it('checking route properties upon valid entry', async (done) => {
+        chai.request('http://localhost:3000')
+            .get('/v1/operators/CIS')
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res).to.have.status(200);
+                chai.expect(res.body).to.have.property('metadata');
+                chai.expect(res.body).to.have.property('data');
+            })
+
+        done();
+    });
+
+    it('checking route properties upon invalid entry', async (done) => {
+        chai.request('http://localhost:3000')
+            .get('/v1/operators/InvalidEntry')
+            .end((err, res) => {
+                chai.expect(err).to.be.null;
+                chai.expect(res).to.have.status(404);
+            })
+
+        done();
+    });
+});
+
