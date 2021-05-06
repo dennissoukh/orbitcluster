bf3e121dfee476238927be0b1260394bc9adfec3
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-05-06 02:13:19 +0100

    fix: navigation menu styling

--- client/src/ui/navigation-menu/Cards/Database/index.tsx
+++ client/src/ui/navigation-menu/Cards/Database/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import styles from '../styles.module.sass';
 
 const ProductsCard = forwardRef<HTMLElement>((props: any, productsRef) => (
-    <section className={styles.card} ref={productsRef}>
+    <section className={styles.card} ref={productsRef} style={{ width: '900px' }}>
         <div className={styles.cardSection}>
             <ul className={styles.navList}>
                 <li>
@@ -33,6 +33,38 @@ const ProductsCard = forwardRef<HTMLElement>((props: any, productsRef) => (
                     </Link>
                 </li>
                 <li className={styles.action}>
+                    <Link to={`/recent/decayed`}>
+                        <div className={styles.icons}>
+                            {/* <IconSatellite/> */}
+                        </div>
+                        <div className={styles.navItem}>
+                            <p className={styles.navItemTitle}>Recently Decayed</p>
+                            <p className={styles.navItemBlurb}>Decayed within the last month</p>
+                        </div>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/categories">
+                        <p className={styles.link}>View all satellite categories</p>
+                    </Link>
+                </li>
+            </ul>
+            <ul className={styles.navList}>
+                <li>
+                    <span className={styles.navTitle}>Objects of Interest</span>
+                </li>
+                <li className={styles.action}>
+                    <Link to={`/categories/brightest`}>
+                        <div className={styles.icons}>
+                            {/* <IconSatellite/> */}
+                        </div>
+                        <div className={styles.navItem}>
+                            <p className={styles.navItemTitle}>Brightest</p>
+                            <p className={styles.navItemBlurb}>Top 100 brightest satellites</p>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.action}>
                     <Link to={`/categories/starlink`}>
                         <div className={styles.icons}>
                             {/* <IconSatellite/> */}
@@ -43,66 +75,54 @@ const ProductsCard = forwardRef<HTMLElement>((props: any, productsRef) => (
                         </div>
                     </Link>
                 </li>
-                <li>
-                    <Link to="/categories">
-                        <p className={styles.link}>View all satellite categories</p>
+                <li className={styles.action}>
+                    <Link to={`/categories/amateur`}>
+                        <div className={styles.icons}>
+                            {/* <IconSatellite/> */}
+                        </div>
+                        <div className={styles.navItem}>
+                            <p className={styles.navItemTitle}>Amateur Radio</p>
+                            <p className={styles.navItemBlurb}>Radio waves from orbit</p>
+                        </div>
+                    </Link>
+                </li>
+                <li className={styles.action}>
+                    <Link to={`/categories/active`}>
+                        <div className={styles.icons}>
+                            {/* <IconSatellite/> */}
+                        </div>
+                        <div className={styles.navItem}>
+                            <p className={styles.navItemTitle}>Active</p>
+                            <p className={styles.navItemBlurb}>Active satellites</p>
+                        </div>
                     </Link>
                 </li>
             </ul>
-            {/* <ul className={styles.navList}>
-                <li>
-                    <span className={styles.navTitle}>Objects of Interest</span>
-                </li>
-                <li className={styles.action}>
-                    <div className={styles.icons}></div>
-                    <div className={styles.navItem}>
-                        <p className={styles.navItemTitle}>Launched</p>
-                        <p className={styles.navItemBlurb}>Launched in the last 30 days</p>
-                    </div>
-                </li>
-                <li className={styles.action}>
-                    <div className={styles.icons}></div>
-                    <div className={styles.navItem}>
-                        <p className={styles.navItemTitle}>Decayed</p>
-                        <p className={styles.navItemBlurb}>Reentered in the last 30 days</p>
-                    </div>
-                </li>
-                <li className={styles.action}>
-                    <div className={styles.icons}></div>
-                    <div className={styles.navItem}>
-                        <p className={styles.navItemTitle}>Brightest</p>
-                        <p className={styles.navItemBlurb}>100 (or so) brightest satellites</p>
-                    </div>
-                </li>
-                <li className={styles.action}>
-                    <div className={styles.icons}></div>
-                    <div className={styles.navItem}>
-                        <p className={styles.navItemTitle}>Amateur Radio</p>
-                        <p className={styles.navItemBlurb}>Radio waves from orbit</p>
-                    </div>
-                </li>
-            </ul> */}
             <ul className={styles.navList}>
                 <li>
                     <span className={styles.navTitle}>Other</span>
                 </li>
                 <li className={styles.action}>
-                    <div className={styles.icons}>
-                        {/* <IconClipboardList/> */}
-                    </div>
-                    <div className={styles.navItem}>
+                    <Link to={`/operators`}>
+                        <div className={styles.icons}>
+                            {/* <IconSatellite/> */}
+                        </div>
+                        <div className={styles.navItem}>
                         <p className={styles.navItemTitle}>Operators</p>
                         <p className={styles.navItemBlurb}>Owners and operators</p>
-                    </div>
+                        </div>
+                    </Link>
                 </li>
                 <li className={styles.action}>
-                    <div className={styles.icons}>
-                        {/* <IconMapPin/> */}
-                    </div>
-                    <div className={styles.navItem}>
+                    <Link to={`/launch-sites`}>
+                        <div className={styles.icons}>
+                            {/* <IconSatellite/> */}
+                        </div>
+                        <div className={styles.navItem}>
                         <p className={styles.navItemTitle}>Launch Sites</p>
                         <p className={styles.navItemBlurb}>Orbital launch sites</p>
-                    </div>
+                        </div>
+                    </Link>
                 </li>
             </ul>
         </div>
