e90fcf19097075e89201d6a8a28dc6f7b989163f
Dennis Soukhikh <dennis.soukhikh@outlook.ie>
2021-05-14 21:24:56 +0100

    fix: various UI fixes

--- client/src/ui/satellite-list-item/SatelliteListItemTitler.tsx
+++ client/src/ui/satellite-list-item/SatelliteListItemTitler.tsx
@@ -3,21 +3,21 @@ import React from 'react';
 export const SatelliteListItemTitler: React.FC = () => {
     return (
         <div className="flex items-center bg-primary-800 text-primary-200 text-sm py-3 px-3 rounded-lg">
-            <div className="w-1/3 sm:w-1/3">
+            <div className="w-full sm:w-3/12">
                 <p>Name</p>
             </div>
-            <div className="w-1/6">
+            <div className="w-5/12 sm:w-1/6">
                 <p>NORAD ID</p>
                 <p>Designator</p>
             </div>
-            <div className="w-1/6">
+            <div className="w-1/4 sm:w-1/6">
                 <p>Object Type</p>
             </div>
-            <div className="w-1/5">
+            <div className="w-1/4 sm:w-1/6">
                 <p>Launch Site</p>
                 <p>Launch Date</p>
             </div>
-            <div className="w-1/6">
+            <div className="w-full sm:w-1/6">
                 <p>Operator</p>
             </div>
             <div className="flex-1 text-right">
